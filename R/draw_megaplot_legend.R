49735375c10a6fc431bfb3099b12c8cc291488ec
stjeske <steffen.jeske.ext@bayer.com>
2025-09-09 12:53:00 +0000

    bug fix: wrong events 'grayed out' when removing or switching events

--- R/draw_megaplot_legend.R
+++ R/draw_megaplot_legend.R
@@ -25,22 +25,14 @@ draw_megaplot_legend <- function(
     heightMar3 <- grconvertY(1, from = 'ndc', to = 'user') - grconvertY(0, from = 'ndc', to = 'user')
     leg.test <- legend(legX, legY, xpd = NA, pch = 15, legend = 'Why', plot = FALSE)
 
-    for (i in 1:length(megaplot_data$event)) {
+    for (i in unique(megaplot_data$event)) {
+      if (!identical(megaplot_data$event.lev[[i]], character(0))) {
       tmp <- megaplot_data$col.ev[[i]]
       col.legtxt <- rep(select_color['plot.id'], length(tmp))
       names(col.legtxt) <- names(tmp)
-      col.legtxt[!names(tmp) %in% megaplot_data$event.lev[[i]]] <- '#5D6A70'
+      col.legtxt[names(tmp)] <- '#5D6A70'
       font.legtxt <- ifelse(col.legtxt == '#5D6A70', 3, 1)
-      col.leg <- megaplot_data$col.ev[[megaplot_data$event[i]]]
-      #  # set text color ('grey' if not shown in the plot)
-      # tmp <- megaplot_data$event.lev[[megaplot_data$event[i]]]
-      # col.legtxt <- rep(select_color['plot.id'], length(tmp))
-      # names(col.legtxt) <- tmp
-      # col.legtxt[!tmp %in% megaplot_data$event.lev[[megaplot_data$event[i]]]] <- '#5D6A70'
-      #
-      # font.legtxt <- ifelse(col.legtxt == '#5D6A70', 3, 1)
-      #
-      # col.leg <- megaplot_data$col.ev[[megaplot_data$event[i]]]
+      col.leg <- megaplot_data$col.ev[[megaplot_data$event.total[i]]]
 
       # event name
       ltitle <- legend(
@@ -54,7 +46,7 @@ draw_megaplot_legend <- function(
         horiz = TRUE,
         col = NA,
         legend = "",
-        text.col = select_color['plot.id'],
+        text.col = "white",
         cex = 1.1,
         text.font = 2
       )
@@ -65,14 +57,14 @@ draw_megaplot_legend <- function(
         cex = 1.1,
         font = 2,
         adj = c(0, 0.5),
-        labels = paste0(megaplot_data$event[i], ': '),
+        labels = paste0(i, ': '), ##
         col = select_color['plot.id']
       )
       legY_num <- legY_num + legY_num_o
       # legend
       lleft <- legX
 
-      for (j in 1:length(col.leg)) {
+      for (j in 1:length(megaplot_data$event.lev[[i]])) {
         l <-
           legend(
             lleft[j],
@@ -81,20 +73,20 @@ draw_megaplot_legend <- function(
             yjust = 0.5,
             xpd = NA,
             bty = 'n',
-            pch = megaplot_data$sym.ev[i],
+            pch = megaplot_data$sym.ev[which(megaplot_data$event %in% i)],
             horiz = TRUE,
-            col = col.leg[j],
-            legend = names(col.leg)[j],
-            text.col = col.legtxt[j],
+            col = megaplot_data$col.ev[[i]][j],
+            legend = megaplot_data$event.lev[[i]][j],
+            text.col = select_color['plot.id'],
             pt.cex = 2.5,
             cex = 1.1,
-            text.font = font.legtxt[j]
+            text.font = 1
           )
         lleft[j + 1] <- l$rect$left + l$rect$w
       }
       # modify y-coordinate for next legend
       legY_num <- legY_num + legY_num_o
-
+      }
     }
   }
 }
