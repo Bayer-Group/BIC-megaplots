0314e313ba0bb6105ad67be7869ae54842cfe03d
stjeske <steffen.jeske.ext@bayer.com>
2026-06-25 10:56:42 +0000

    Bug fix: Colour picker panel won't disappear after deselecting event in jsTree fixed

--- R/mod_color_selection.R
+++ R/mod_color_selection.R
@@ -354,6 +354,7 @@ mod_color_selection_server <- function(
 
     shiny::observeEvent(checked_data$val, {
       if (is.null(checked_data$val) || nrow(checked_data$val) == 0) {
+        shinyjs::hideElement(id = "colour_picker_panel")
         shinyjs::hideElement(id = "selected_events_color_container_panel")
       } else {
         shinyjs::showElement(id = "selected_events_color_container_panel")
